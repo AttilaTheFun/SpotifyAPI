92d2c8e6c89b62c89a21d95f3179e51f62833e16
Logan Shire <logan.shire@gmail.com>
2023-05-14 15:01:38 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,10 +12,6 @@ let package = Package(
             name: "SpotifyAPI",
             targets: ["SpotifyWebAPI", "SpotifyExampleContent"]
         ),
-        .library(
-            name: "_SpotifyAPITestUtilities",
-            targets: ["SpotifyAPITestUtilities"]
-        )
     ],
     dependencies: packageDependencies,
     targets: [
@@ -39,27 +35,6 @@ let package = Package(
                 .process("Resources")
             ]
         ),
-        .target(
-            name: "SpotifyAPITestUtilities",
-            dependencies: spotifyAPITestUtilitiesDependencies,
-            exclude: ["README.md"],
-            resources: [
-                .process("Resources")
-            ]
-        ),
-        
-        // MARK: Test Targets
-        
-        .testTarget(
-            name: "SpotifyAPIMainTests",
-            dependencies: [
-                "SpotifyWebAPI",
-                "SpotifyExampleContent",
-                "RegularExpressions",
-                "SpotifyAPITestUtilities"
-            ]
-        )
-        
     ]
 )
 
@@ -91,50 +66,6 @@ var packageDependencies: [Package.Dependency] {
             from: "1.0.0"
         )
     ]
-    
-    #if TEST
-    dependencies += [
-        .package(
-            name: "vapor",
-            url: "https://github.com/vapor/vapor.git",
-            from: "4.45.3"
-        ),
-        .package(
-            name: "swift-nio",
-            url: "https://github.com/apple/swift-nio.git",
-            from: "2.27.0"
-        ),
-        .package(
-            name: "async-http-client",
-            url: "https://github.com/swift-server/async-http-client.git",
-            from: "1.2.5"
-        )
-    ]
-    #endif
 
     return dependencies
-}
-
-var spotifyAPITestUtilitiesDependencies: [Target.Dependency] {
-    
-    var dependencies: [Target.Dependency] = [
-        "SpotifyWebAPI",
-        "SpotifyExampleContent",
-        .product(name: "RegularExpressions", package: "RegularExpressions"),
-        .product(name: "OpenCombine", package: "OpenCombine"),
-        .product(name: "OpenCombineDispatch", package: "OpenCombine"),
-        .product(name: "OpenCombineFoundation", package: "OpenCombine")
-    ]
-    
-    #if TEST
-    dependencies += [
-        .product(name: "Vapor", package: "vapor"),
-        .product(name: "NIOHTTP1", package: "swift-nio"),
-        .product(name: "NIO", package: "swift-nio"),
-        .product(name: "AsyncHTTPClient", package: "async-http-client")
-    ]
-    #endif
-    
-    return dependencies
-
 }
